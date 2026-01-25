f3ca01a4574af6de9d61665f697039ff01742d94
Florent Gallaire <fgallaire@gmail.com>
2026-01-25 06:17:00 +0100

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,19 @@
+"""Module providing the solver translations."""
+
+T = {
+    "en": {"purple": "purple", "yellow": "yellow", "green": "green", "blue": "blue", "red": "red",
+           "orange": "orange", "maroon": "maroon", "white": "white", "fuchsia": "fuchsia",
+           "left": "left", "right": "right", "up": "up", "down": "down"},
+    "fr": {"purple": "violet", "yellow": "jaune", "green": "vert", "blue": "bleu", "red": "rouge",
+           "orange": "orange", "maroon": "marron", "white": "blanc", "fuchsia": "rose",
+           "left": "gauche", "right": "droite", "up": "haut", "down": "bas"},
+    "es": {"purple": "púrpura", "yellow": "amarillo", "green": "verde", "blue": "azul", "red": "rojo",
+           "orange": "naranja", "maroon": "marrón", "white": "blanco", "fuchsia": "rosa",
+           "left": "izquierda", "right": "derecha", "up": "arriba", "down": "abajo"},
+    "vi": {"purple": "tím", "yellow": "vàng", "green": "xanh lá", "blue": "xanh dương", "red": "đỏ",
+           "orange": "cam", "maroon": "nâu", "white": "trắng", "fuchsia": "hồng",
+           "left": "trái", "right": "phải", "up": "lên", "down": "xuống"},
+    "it": {"purple": "viola", "yellow": "giallo", "green": "verde", "blue": "blu", "red": "rosso",
+           "orange": "arancione", "maroon": "marrone", "white": "bianco", "fuchsia": "rosa",
+           "left": "sinistra", "right": "destra", "up": "alto", "down": "basso"},
+    }
